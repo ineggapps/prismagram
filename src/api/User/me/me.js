37eecc52f34dabe5b78c0253d958f5b420c50fde
ineggapps <inegg.apps@gmail.com>
2019-07-12 13:05:04 +0900

    Computed fiels has been added in me
    fullName has been composed firstName and lastName of user profile.

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -7,12 +7,17 @@ export default {
     me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      const userProfile = await prisma.user({ id: user.id });
       const posts = await prisma.user({ id: user.id }).posts();
       return {
         user: userProfile,
         posts
       };
+    }
+  },
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
     }
   }
 };
