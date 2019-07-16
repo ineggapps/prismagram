c257ef6a8117a58634733eeb74af49c17050d465
ineggapps <inegg.apps@gmail.com>
2019-07-16 20:06:51 +0900

    newMessage of subscription has been implemented!

---
+++ src/api/Message/newMessage/newMessage.js
@@ -0,0 +1,17 @@
+import { prisma } from "./../../../../generated/prisma-client";
+export default {
+  Subscription: {
+    newMessage: {
+      subscribe: (_, args) => {
+        const { roomId } = args;
+        console.log(roomId);
+        return prisma.$subscribe
+          .message({
+            AND: [{ mutation_in: "CREATED" }, { node: { room: { id: roomId } } }]
+          })
+          .node();
+      },
+      resolve: payload => payload
+    }
+  }
+};
