948f2455c3992c513afa455e9299d06f10b8adaf
ineggapps <inegg.apps@gmail.com>
2019-07-20 16:58:21 +0900

    Some of queries has been updated.

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,8 +4,15 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({ username, email, firstName, lastName, bio });
-      return user;
+      const exists = await prisma.$exists.user({
+        OR: [{ username }, { email }]
+      });
+      if (exists) {
+        throw Error("This username / email is already taken.");
+        return;
+      }
+      await prisma.createUser({ username, email, firstName, lastName, bio });
+      return true;
     }
   }
 };
