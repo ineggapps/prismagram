682faade7a7eff4296fb8dca18df1f5841d278d1
ineggapps <inegg.apps@gmail.com>
2019-07-24 15:13:30 +0900

    seeUser method has been modified.

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -2,13 +2,9 @@ import { prisma } from "./../../../../generated/prisma-client";
 export default {
   Query: {
     seeUser: async (_, args, { request }) => {
-      const { id } = args;
-      const user = await prisma.user({ id });
-      const posts = await prisma.user({ id }).posts();
-      return {
-        user,
-        posts
-      };
+      const { username } = args;
+      return prisma.user({ username });
+      // const posts = await prisma.user({ username }).posts();
     }
   }
 };
